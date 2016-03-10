b186aef4ee2f227f4eb79bde85d9a72734f55d94
Brian Dentino <brian.dentino@gmail.com>
2016-03-10 14:12:48 -0800

    add drop, compression, and upgrade to kev 0.3 compatibility

--- index.js
+++ index.js
@@ -1,76 +1,119 @@
-var redis = require('redis')
+var Promise = require('bluebird')
+var seconds = require('juration').parse
+var resurrect = require('./resurrect')()
+var zlib = require('zlib')
 
-var KevRedis = module.exports = function KevRedis(options) {
+var Redis = require('redis')
+Promise.promisifyAll(Redis.RedisClient.prototype)
+Promise.promisifyAll(Redis.Multi.prototype)
+
+var KevRedis = module.exports = function KevRedis (options) {
   if (!(this instanceof KevRedis)) return new KevRedis(options)
 
   options = options || {}
   options.port = options.port || 6379
   options.host = options.host || '127.0.0.1'
-  var client = redis.createClient(options.port, options.host, options)
+  options.compress = options.compress || false
+  var client = Redis.createClient(options.port, options.host, options)
+
+  if (options.ttl) options.ttl = seconds(String(options.ttl))
+  if (options.prefix) options.prefix = options.prefix + ':'
+  else options.prefix = ''
+  this.options = options
 
   this.pendingOps = []
-  this.ttl = options.ttl
-  var self = this
-  client.on('connect', function() {
-    self.storage = client;
-    for (var index in self.pendingOps) {
-      self.pendingOps[index]()
+  client.on('connect', () => {
+    this.storage = client;
+    for (var index in this.pendingOps) {
+      this.pendingOps[index]()
     }
   })
 }
 
-KevRedis.prototype.get = function(key, done) {
-  if (!this.storage) return this.pendingOps.push(this.get.bind(this, key, done))
+KevRedis.prototype.get = function (keys, done) {
+  if (!this.storage) return this.pendingOps.push(this.get.bind(this, keys, done))
+  keys = keys.map((k) => this.options.prefix + k)
+  this.storage.mgetAsync(keys)
+    .reduce((out, v, idx) => { out[keys[idx]] = unpack(this.options.compress)(v); return out }, {})
+    .props()
+    .then((out) => done && done(null, out))
+    .catch((err) => done && done(err))
+}
 
-  if (Array.isArray(key)) {
-    var out = {}
-    this.storage.mget(key, function (err, result) {
-      result.forEach(function (v, idx) {
-        out[key[idx]] = unpack(v)
-      })
-      done(err, out)
-    })
-  } else {
-    this.storage.get(key, function(err, result) {
-      done(err, unpack(result))
-    })
+KevRedis.prototype.put = function (keys, options, done) {
+  if (!this.storage) return this.pendingOps.push(this.put.bind(this, keys, options, done))
+
+  var ttl = options.ttl || this.options.ttl
+  for (var key in keys) {
+    key = this.options.prefix + key
+    keys[key] = pack(this.options.compress)(keys[key])
+      .then((v) => this.storage.getsetAsync(key, v))
+      .tap((v) => ttl && this.storage.expire(key, ttl))
+      .then(unpack(this.options.compress))
   }
+
+  Promise.props(keys)
+    .then((v) => done && done(null, v))
+    .catch((err) => done && done(err))
 }
 
-KevRedis.prototype.put = function(key, value, done) {
-  if (!this.storage) return this.pendingOps.push(this.put.bind(this, key, value, done))
+KevRedis.prototype.del = function (keys, done) {
+  keys = keys.map((k) => this.options.prefix + k)
+  if (!this.storage) return this.pendingOps.push(this.del.bind(this, keys, done))
 
-  var ttl = this.ttl
-  var storage = this.storage
-  this.storage.getset(key, pack(value), function(err, result) {
-    if (ttl) storage.expire(key, ttl)
-    if (done) done(err, unpack(result))
-  })
+  this.storage.multi().mget(keys).del(keys).execAsync()
+    .then((out) => out[0])
+    .reduce((p, c, i) => { p[keys[i]] = unpack(this.options.compress)(c); return p }, {})
+    .props()
+    .then((v) => done && done(null, v))
+    .catch((e) => done && done(e))
 }
 
-KevRedis.prototype.del = function(key, done) {
-  if (!this.storage) return this.pendingOps.push(this.del.bind(this, key, done))
-
-  var store = this.storage;
-  this.get(key, function(err, value) {
-    if (err) return done(err)
-
-    store.del(key, function(err) {
-      if (err) return done(err)
-
-      done(null, value)
-    })
-  })
+KevRedis.prototype.drop = function (pattern, done) {
+  pattern = this.options.prefix + pattern
+  if (!this.storage) return this.pendingOps.push(this.drop.bind(this, pattern, done))
+  this.storage.evalAsync(
+    `local keys = redis.call('keys', '${pattern}') \n` +
+    `for i=1,#keys,5000 do \n` +
+    `  redis.call('del', unpack(keys, i, math.min(i+4999, #keys))) \n` +
+    `end \n` +
+    `return keys`
+  , 0)
+  .then((keys) => done && done(null, keys.length))
+  .catch((e) => done && done(null, err))
 }
 
-KevRedis.prototype.close = function(done) {
+KevRedis.prototype.close = function (done) {
   if (!this.storage) return this.pendingOps.push(this.close.bind(this, done))
-
   if (!this.storage.connected) { return process.nextTick(done) }
-
   this.storage.once('end', done || function() {})
   this.storage.quit()
 }
 
-function pack(value) { return JSON.stringify(value) }
-function unpack(value) { return JSON.parse(value) }
+function pack (compress) {
+  return Promise.promisify((value, done) => {
+    if (!value) return setImmediate(done)
+    if (!compress) {
+      setImmediate(() => done(null, resurrect.stringify(value)))
+    } else {
+      zlib.deflate(resurrect.stringify(value), compress, (err, buf) => {
+        if (err) done(err)
+        else done(null, buf.toString('base64'))
+      })
+    }
+  })
+}
+
+function unpack (compress) {
+  return Promise.promisify((value, done) => {
+    if (!value) return setImmediate(done)
+    if (!compress) {
+      setImmediate(() => done(null, resurrect.resurrect(value)))
+    } else {
+      zlib.inflate(new Buffer(value, 'base64'), compress, (err, val) => {
+        if (err) done(err)
+        else done(null, resurrect.resurrect(val.toString()))
+      })
+    }
+  })
+}
--- test/test.js
+++ test/test.js
@@ -1,52 +1,23 @@
 var Kev = require('kev')
+var test_core = require('kev/test/test-plugin-core')
+var test_ttl = require('kev/test/test-ttl')
 var KevRedis = require('../index.js')
 var Promise = require('bluebird')
 var assert = require('assert')
 
-var kev = Promise.promisifyAll(Kev({ store: KevRedis( { port: process.env.REDIS_PORT, ttl: 5 } ) }))
-
-kev.putAsync('key1', 'value1').then(function() {
-  return kev.getAsync('key1')
-}).then(function(value) {
-  assert.equal(value, 'value1')
-}).then(function () {
-  return kev.delAsync('key1')
-}).then(function(old) {
-  assert.equal(old, 'value1')
-}).then(function () {
-  return kev.getAsync('key1')
-}).then(function (value) {
-  assert.equal(value, null)
-}).then(function () {
-  var max = 2000
-  var puts = []
-  for (var i = 1; i <= max; i++) {
-    puts.push(kev.putAsync(String(i), i))
-  }
-  return Promise.all(puts)
-}).then(function () {
-  console.log('put all')
-  var max = 2000
-  var keys = []
-  for (var i = 1; i <= max; i++) {
-    keys.push(String(i))
-  }
-  return kev.getAsync(keys)
-}).then(function (values) {
-  console.log('got all')
-  assert.equal(values['100'], 100)
-  assert.equal(values['2000'], 2000)
-}).then(function (values) {
-  return kev.putAsync('key2', 'to-expire')
-}).then(function () {
-  return kev.getAsync('key2')
-}).then(function (value) {
-  assert.equal(value, 'to-expire')
-  return Promise.delay(6000)
-}).delay(6000).then(function () {
-  return kev.getAsync('key2')
-}).then(function(value) {
-  assert.equal(value, null)
-  kev.close()
-  console.log('PASSED')
+var core = KevRedis({
+  port: process.env.REDIS_PORT,
+  ttl: 5
 })
+
+var compressed = KevRedis({
+  port: process.env.REDIS_PORT,
+  ttl: 5,
+  compress: true
+})
+
+Promise.resolve()
+  .then(() => test_core(core))
+  .then(() => test_ttl(core))
+  .then(() => test_core(compressed))
+  .then(() => test_ttl(compressed))
